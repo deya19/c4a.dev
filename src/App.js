11481f0942d47d34aff2062f75c23cb48de7fb01
deya19 <deyasaleh2@gmail.com>
2022-09-18 14:05:06 +0300

    edit app.js file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+  <>
+         <h1>version 2</h1>
+         <h1 className="title">uygyugyyg</h1>
+         <h2 className="">ijkjij</h2>
+      
+  </>
+  );
+}
+
+export default App;
